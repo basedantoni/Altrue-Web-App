49b667eab9ca61d0cb4fe9426574e0bb2dd019f7
basedantoni <anthony.mercado300@gmail.com>
2019-10-14 17:16:52 -0500

    added react app under client folder

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://amercad3:passwd@cluster0-5u1il.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://amercad3:!$y0@b11!g0@altrue-5u1il.mongodb.net/test?retryWrites=true&w=majority'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require("../../config/keys");
+
+// Input Validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // User Model
 const User = require('../../models/User');
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post('/register', (req, res) => {
+  // Form validation
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists'});
+    }
+    else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 // @route GET api/users
 // @desc Get All users
