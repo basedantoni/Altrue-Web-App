07b7cd23be09171e841eaa35c35fff030fa6b2de
basedantoni <anthony.mercado300@gmail.com>
2019-10-29 17:03:20 -0500

    removed url from axios in authActions

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -23,7 +23,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post("http://localhost:5000/api/users/login", userData)
+    .post("/api/users/login", userData)
     .then(res => {
     // Save to localStorage
     //set token to localStorage
