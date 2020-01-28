9cb50cfe6b80422df1cf4b8b93702c48825c6d9c
basedantoni <anthony.mercado300@gmail.com>
2020-01-28 14:21:54 -0600

    node modules added to git ignore

--- client/src/components/CreateEvents.js
+++ client/src/components/CreateEvents.js
@@ -11,7 +11,7 @@ class CreateEvents extends Component {
           location: "",
           date: "",
           time: "",
-          //toDashboard: false,
+          toDashboard: false,
           //errors: {}
         };
     }
